8dcd7775a396c100183725e73650e9f2e800f65c
Maksymilian Tym <tymszczur@gmail.com>
2018-12-09 19:37:42 +0100

    Added partial slider scripting
    - Added a slider script
    - Sliders can be edited only in edit mode now
    - Added an integer for the sliders

--- project/Idea Saving Software/Assets/Scripts/EditButton.cs
+++ project/Idea Saving Software/Assets/Scripts/EditButton.cs
@@ -10,13 +10,17 @@ public class EditButton : MonoBehaviour {
 	public InputField title;
 	public Text pholderDescription;
 	public Text pholderTitle;
+	public Sliders slidersCS;
 
 	void Start(){
 		title = transform.GetChild(0).GetComponent<InputField>();
 		description = transform.GetChild(1).GetComponent<InputField>();
 		title.interactable = false;
 		description.interactable = false;
-
+		slidersCS = GameObject.Find("Sliders Empty").GetComponent<Sliders>();
+		slidersCS.difficultySlider.interactable = false;
+		slidersCS.confidenceSlider.interactable = false;
+		slidersCS.impactSlider.interactable = false;
 	}
 
 
@@ -28,14 +32,20 @@ public class EditButton : MonoBehaviour {
 			isEditing = false;
 			title.interactable = false;
 			description.interactable = false;
-			transform.GetChild(5).GetComponent<Image>().color = Color.white;
+			transform.GetChild(3).GetComponent<Image>().color = Color.white;
+			slidersCS.difficultySlider.interactable = false;
+			slidersCS.confidenceSlider.interactable = false;
+			slidersCS.impactSlider.interactable = false;
 		}
 		else
 		{
 			isEditing = true;
 			title.interactable = true;
 			description.interactable = true;
-			transform.GetChild(5).GetComponent<Image>().color = Color.green;
+			transform.GetChild(3).GetComponent<Image>().color = Color.green;
+			slidersCS.difficultySlider.interactable = true;
+			slidersCS.confidenceSlider.interactable = true;
+			slidersCS.impactSlider.interactable = true;
 		}
 
 	}
---
+++ project/Idea Saving Software/Assets/Scripts/Sliders.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Sliders : MonoBehaviour {
+
+	public Slider difficultySlider;
+	public Slider confidenceSlider;
+	public Slider impactSlider;
+	public int difficulty;
+	public int confidence;
+	public int impact;
+
+
+	// Use this for initialization
+	void Start () {
+		difficultySlider = transform.GetChild(0).GetComponent<Slider>();
+		impactSlider = transform.GetChild(1).GetComponent<Slider>();
+		confidenceSlider = transform.GetChild(2).GetComponent<Slider>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		difficulty = (int)difficultySlider.value;
+		confidence = (int)confidenceSlider.value;
+		impact = (int)impactSlider.value;
+	}
+}
