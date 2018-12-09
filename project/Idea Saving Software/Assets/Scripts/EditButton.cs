512f1486005f47b60de6f24aae9456c54f40145c
Maksymilian Tym <tymszczur@gmail.com>
2018-12-09 19:01:27 +0100

    Added some UI
    - Added 3 fonts
    - Added a idea panel with some shit
    - Added 3 sliders onto the panel
    - Added 2 scripted text fields
    - Added an edit button

---
+++ project/Idea Saving Software/Assets/Scripts/EditButton.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EditButton : MonoBehaviour {
+
+	public bool isEditing = false;
+	public InputField description;
+	public InputField title;
+	public Text pholderDescription;
+	public Text pholderTitle;
+
+	void Start(){
+		title = transform.GetChild(0).GetComponent<InputField>();
+		description = transform.GetChild(1).GetComponent<InputField>();
+		title.interactable = false;
+		description.interactable = false;
+
+	}
+
+
+	
+	public void OnEditClick()
+	{
+		if(isEditing == true)
+		{
+			isEditing = false;
+			title.interactable = false;
+			description.interactable = false;
+			transform.GetChild(5).GetComponent<Image>().color = Color.white;
+		}
+		else
+		{
+			isEditing = true;
+			title.interactable = true;
+			description.interactable = true;
+			transform.GetChild(5).GetComponent<Image>().color = Color.green;
+		}
+
+	}
+
+}
